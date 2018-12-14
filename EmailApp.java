a772ef436c28ff196b6d17a4c4b27ffbb04462fb
Afshan Fatima <fafshan09@gmail.com>
2018-12-14 15:41:54 -0700

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,24 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Email em1 = new Email("Afshan","Fatima");
+         System.out.println(em1.showInfo());
+         
+       //   em1.changePassword(password);
+         
+       //  em1.setMailboxCapacity(capacity);
+         
+       //  Example here   
+       //  em1.setAlternateEmail(altEmail);
+       // em1.setAlternateEmail("rs@gmail.com");
+       //System.out.println(em1.getAlternateEmail());
+	  
+  
+        
+	
+	}
+	
+}
